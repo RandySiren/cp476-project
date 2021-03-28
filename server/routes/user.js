0abffb0b2bf7712086063dec2c689e578a779147
AndrewChoi001 <72709903+AndrewChoi001@users.noreply.github.com>
2021-03-28 14:19:45 -0400

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,6 +4,9 @@ import { login, signup } from '../controllers/user.js'
 const router = express.Router()
 
 router.post('/login', login)
+router.get('/login',(req,res)=>{
+  res.sendFile(../../client/src/pages/login.html);
+})
 router.post('/signup', signup)
 
 export default router
